ccaf50c19a53b06bbfa2d94d3f48bd8cb81eaebe
Anastasia <coca-cola23-3@mail.ru>
2017-11-02 06:37:46 +0300

    Before change workflow

---
+++ Task2/Class.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Crm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+
+namespace WorkflowZA
+{
+    public class Class1 : CodeActivity
+    {
+        string configEntity = "scnsoft_autonumberaz";
+        string entityName = "scnsoft_securitypaper";
+              
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            var context = executionContext.GetExtension<IWorkflowContext>();
+
+
+            var serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            var queryConfig = new QueryExpression(this.configEntity) { ColumnSet = new ColumnSet(true) };
+            var queryEntity = new QueryExpression(entityName) { ColumnSet = new ColumnSet(true) };
+
+            //query.Criteria.AddCondition(entityName, "scnsoft_name", ConditionOperator.Contains, "acc");
+            //query.Criteria.AddCondition(entityName, "scnsoft_name", ConditionOperator.Contains, "tst");
+
+            var filterAr = new FilterExpression(LogicalOperator.And);
+            filterAr.AddCondition(entityName, "scnsoft_name", ConditionOperator.DoesNotContain, "acc");
+            filterAr.AddCondition(entityName, "scnsoft_name", ConditionOperator.DoesNotContain, "tst");
+            queryEntity.Criteria.AddFilter(filterAr);
+
+            var resultEntity = service.RetrieveMultiple(queryEntity);
+            var configEntity = service.RetrieveMultiple(queryConfig).Entities.FirstOrDefault();
+
+            int uniqueNumber = configEntity.GetAttributeValue<int>("scnsoft_uniquenumber");
+            string prefix = configEntity.GetAttributeValue<string>("scnsoft_prefix");
+            string postfix = configEntity.GetAttributeValue<string>("scnsoft_postfix");
+            int increment = configEntity.GetAttributeValue<int>("scnsoft_increment");
+
+
+            if (resultEntity != null)   
+            {
+                foreach(var entity in resultEntity.Entities)
+                {
+                    entity["scnsoft_name"] = prefix + "-" + uniqueNumber + "-" + postfix;
+                    uniqueNumber += increment;
+
+                    configEntity["scnsoft_unique"] = uniqueNumber;
+                    service.Update(configEntity);
+                    service.Update(entity);                    
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System.Linq;
+
+namespace Task2
+{
+    public class AccountNumberPlugin : IPlugin
+    {
+        // CrmServiceClient service = new CrmServiceClient(
+        //        "AuthType=AD;Url=https://studdev.scnsoft.com/studdev; Domain=MAIN; Username=crm-test17@scnsoft.com; Password=Abcd1234");
+
+        string entityName = "scnsoft_securitypaper";
+        string fieldName = "scnsoft_name";
+        
+
+        private string GetNameEntity(Entity ConfigEntity, out int uniqueNumber)
+        {
+            uniqueNumber = ConfigEntity.GetAttributeValue<int>("scnsoft_uniquenumber");
+            string prefix = ConfigEntity.GetAttributeValue<string>("scnsoft_prefix");            
+            string postfix = ConfigEntity.GetAttributeValue<string>("scnsoft_postfix");
+
+            return (prefix + "-" + uniqueNumber.ToString() + "-" + postfix);
+        }
+
+        private void SetUniqueNumber(Entity ConfigEntity, int uniqueNumber, IOrganizationService service)
+        {
+            int increment = ConfigEntity.GetAttributeValue<int>("scnsoft_increment");
+
+            uniqueNumber += increment;
+
+            ConfigEntity["scnsoft_uniquenumber"] = uniqueNumber;
+            service.Update(ConfigEntity);
+        }
+       
+        private Entity CreateNewEntity(IOrganizationService service)
+        {
+            var autonumberAZ = new Entity("scnsoft_autonumberaz");
+
+            autonumberAZ.Id = Guid.NewGuid();
+            autonumberAZ["scnsoft_prefix"] = "acc";
+            autonumberAZ["scnsoft_uniquenumber"] = "001";
+            autonumberAZ["scnsoft_increment"] = 1;
+            autonumberAZ["scnsoft_postfix"] = "tst";
+            autonumberAZ["scnsoft_apply"] = entityName;
+            autonumberAZ["scnsoft_applytofield"] = fieldName;
+
+            service.Create(autonumberAZ);
+
+            return autonumberAZ;
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the execution context from the service provider.
+            Microsoft.Xrm.Sdk.IPluginExecutionContext context = (Microsoft.Xrm.Sdk.IPluginExecutionContext)
+                serviceProvider.GetService(typeof(Microsoft.Xrm.Sdk.IPluginExecutionContext));
+
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+            var query = new QueryExpression("scnsoft_autonumberaz") { ColumnSet = new ColumnSet(true) };
+            var ConfigEntity = service.RetrieveMultiple(query).Entities.FirstOrDefault();
+
+            if (ConfigEntity == null)
+                ConfigEntity = CreateNewEntity(service);
+            
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+
+                //// Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+                ////</snippetAccountNumberPlugin2>
+
+                //// Verify that the target entity represents an account.
+                //// If not, this plug-in was not registered correctly.
+                if (entity.LogicalName == entityName)
+                {
+                    int uniqueNumber;
+                    entity[fieldName] = GetNameEntity(ConfigEntity, out uniqueNumber);
+
+
+                    SetUniqueNumber(ConfigEntity, uniqueNumber, service);
+                    
+                }
+            }
+        }
+    }
+}
+
